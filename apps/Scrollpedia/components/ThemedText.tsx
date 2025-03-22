6b04ebc67da9b9165809b9efdb21cf3c48cdad51
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2025-03-22 13:40:25 +0530

    Add Inter font support and update themed text component

--- apps/Scrollpedia/components/ThemedText.tsx
+++ apps/Scrollpedia/components/ThemedText.tsx
@@ -20,6 +20,7 @@ export function ThemedText({
   return (
     <Text
       style={[
+        {fontFamily: 'Inter_400Regular'},
         { color },
         type === 'default' ? styles.default : undefined,
         type === 'title' ? styles.title : undefined,
