09086489386235504ebc6b3eb4b6ab4f8ed387e9
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2025-03-29 13:14:32 +0530

    Add CLOUDINARY_CLOUD_NAME constant to environment variables

--- services/content-summarization-service/constants/Constants.py
+++ services/content-summarization-service/constants/Constants.py
@@ -10,3 +10,4 @@ class Constants:
     AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_KEY')
     REGION_NAME = os.getenv('REGION_NAME', 'us-west-2')  # Default to 'us-west-2' if not set
     GROQ_KEY = os.getenv('GROQ_KEY')
+    CLOUDINARY_CLOUD_NAME = os.getenv('CLOUDINARY_CLOUD_NAME')
